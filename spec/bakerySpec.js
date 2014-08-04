da3160db8c011d5dcdbc515eb33633d653b31d51
Catherine O'Neill <cathzozo@gmail.com>
2014-08-03 19:10:50 -0500

    3 tests for buy function passing

--- spec/bakerySpec.js
+++ spec/bakerySpec.js
@@ -1,36 +1,36 @@
 describe("Logic......................", function() {
 
+	describe("buy function", function () {
+		it("records the purchase of ingredients", function() {
+			logic.buy("flour", 6, 10);
+			logic.buy("eggs", 12, 12);
+			expect(bakery.supplies.flour).toEqual(14);
+			expect(bakery.supplies.eggs).toEqual(24);
+		});
 
-	it("should be able to record the purchase of ingredients", function() {
-		logic.buy("flour", 6, 10);
-		logic.buy("eggs", 12, 12);
-		expect(bakery.supplies.flour).toEqual(14);
-		expect(bakery.supplies.eggs).toEqual(24);
+	    it("subtracts the amount of money spent on purchasing ingredients", function() {
+	      	logic.buy("flour", 100, 10);
+			logic.buy("eggs", 85, 12);
+			expect(bakery.money).toEqual(815);
+
+	    });
+	
+	    it("returns false if there are insufficient funds to make a purchase, otherwise true", function() {
+	      	var sufficientFunds = logic.buy("flour", 900, 10);
+			var insufficientFunds = logic.buy("eggs", 200, 12);
+			expect(sufficientFunds).toEqual(true);
+			expect(insufficientFunds).toEqual(false);
+	    });
 	});
 
-    it("should subtract the amount of money spent on purchasing ingredients", function() {
-      	logic.buy("flour", 100, 10);
-		logic.buy("eggs", 85, 12);
-		expect(bakery.money).toEqual(815);
+	xit("tells the current song if the user has made it a favorite", function() {
+	    spyOn(song, 'persistFavoriteStatus');
 
-    });
+	    player.play(song);
+	    player.makeFavorite();
 
-    xit("should be possible to resume", function() {
-      player.resume();
-      expect(player.isPlaying).toBeTruthy();
-      expect(player.currentlyPlayingSong).toEqual(song);
-    });
-
-
-  // demonstrates use of spies to intercept and test method calls
-  xit("tells the current song if the user has made it a favorite", function() {
-    spyOn(song, 'persistFavoriteStatus');
-
-    player.play(song);
-    player.makeFavorite();
-
-    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  });
+	    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
+	});
 
   //demonstrates use of expected exceptions
   describe("#resume", function() {

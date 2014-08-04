a929275a1a236b90c38745f9a82b91612e34f667
Catherine O'Neill <cathzozo@gmail.com>
2014-08-03 19:01:54 -0500

    2 tests passed

--- spec/bakerySpec.js
+++ spec/bakerySpec.js
@@ -1,24 +1,18 @@
 describe("Logic......................", function() {
 
 
-  it("should be able to record the purchase of ingredients", function() {
-    logic.buy("flour", 6, 10)
-    logic.buy("eggs", 12, 12)
-    expect(bakery.supplies.flour).toEqual(14);
-    expect(bakery.supplies.eggs).toEqual(24);
-  });
+	it("should be able to record the purchase of ingredients", function() {
+		logic.buy("flour", 6, 10);
+		logic.buy("eggs", 12, 12);
+		expect(bakery.supplies.flour).toEqual(14);
+		expect(bakery.supplies.eggs).toEqual(24);
+	});
 
-  describe("when song has been paused", function() {
-    beforeEach(function() {
-      player.play(song);
-      player.pause();
-    });
+    it("should subtract the amount of money spent on purchasing ingredients", function() {
+      	logic.buy("flour", 100, 10);
+		logic.buy("eggs", 85, 12);
+		expect(bakery.money).toEqual(815);
 
-    xit("should indicate that the song is currently paused", function() {
-      expect(player.isPlaying).toBeFalsy();
-
-      // demonstrates use of 'not' with a custom matcher
-      expect(player).not.toBePlaying(song);
     });
 
     xit("should be possible to resume", function() {
@@ -26,7 +20,7 @@ describe("Logic......................", function() {
       expect(player.isPlaying).toBeTruthy();
       expect(player.currentlyPlayingSong).toEqual(song);
     });
-  });
+
 
   // demonstrates use of spies to intercept and test method calls
   xit("tells the current song if the user has made it a favorite", function() {

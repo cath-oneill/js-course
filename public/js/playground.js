7cb846f9e1cfcc1bf4e5ff495e4ffab64509f106
Catherine O'Neill <cathzozo@gmail.com>
2014-08-27 14:16:00 -0500

    step 2 done

--- public/js/playground.js
+++ public/js/playground.js
@@ -1,13 +1,43 @@
 var endpointCount = 77;
+var arrayOfObjects = [];
+var counter = 0;
+
+
 for (var i = 0; i < endpointCount; i++) {
-	// Make a call to our server, requesting a set of data.
-	// The data we get back is a chunk of strings that form
-	// a larger message.
+	getData(i);
+}
+
+function getData(i) {
 	$.ajax({
 		type: 'GET',
 		url: '/' + i,
-		success: function() {
-			console.log('Got back success from call ' + i + '!');
+		success: function(reply) {
+			var json = JSON.parse(reply);
+			if ($.isEmptyObject(json) === false) {
+				addToArray(json, i);
+			}
+			counter ++;
+			if (counter === endpointCount) {
+				text = arrayOfObjects.join("");
+				console.log(text);
+				$('#container').append(text);
+			}
 		}
 	});
+
 }
+
+function addToArray(json, i) {
+	if (json["1"] === undefined) {
+		string = json["0"];
+	} else if (json["2"] === undefined) {
+		string = json["0"] + json["1"]; 
+	} else {
+		string = json["0"] + json["1"] + json["2"];
+		console.log(string);
+	}
+	arrayOfObjects[i] = string.replace(/\n/g, "<br />");
+}
+
+
+
